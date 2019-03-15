231bd1e1a7083315fd432b34956711e5c1628bcc
ana galvez <a.galvezsalas@gmail.com>
2019-03-15 00:21:31 -0300

    Updates

---
+++ precache-manifest.5ea332b772edf6f47442e630c81fdf5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0d5675ecb36ef66deda",
+    "url": "/burguer-queen/static/js/runtime~main.36f0585b.js"
+  },
+  {
+    "revision": "879e6a12d5293c24309b",
+    "url": "/burguer-queen/static/js/main.efcb3d40.chunk.js"
+  },
+  {
+    "revision": "1b3513bf0f11d68127ec",
+    "url": "/burguer-queen/static/js/2.8adf4355.chunk.js"
+  },
+  {
+    "revision": "879e6a12d5293c24309b",
+    "url": "/burguer-queen/static/css/main.9e8e5696.chunk.css"
+  },
+  {
+    "revision": "7b77bc0a7461d0882bca5203aeb8f555",
+    "url": "/burguer-queen/index.html"
+  }
+];
